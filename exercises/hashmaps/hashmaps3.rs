2288ccadb8cff8cd1d0bed79d468bc5616a29bc4
Yayuit <33405544+Yayuit@users.noreply.github.com>
2025-03-28 07:26:06 +0000

    hashmap and modules

--- exercises/hashmaps/hashmaps3.rs
+++ exercises/hashmaps/hashmaps3.rs
@@ -14,14 +14,29 @@
 // Execute `rustlings hint hashmaps3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 use std::collections::HashMap;
 
 // A structure to store the goal details of a team.
 struct Team {
+    name:String,
     goals_scored: u8,
     goals_conceded: u8,
+}
+
+impl Team {
+    fn new(name: String) -> Self {
+        Team {
+            name,
+            goals_scored: 0,
+            goals_conceded: 0,
+        }
+    }
+
+    fn update_scores(&mut self, scored: u8, conceded: u8) {
+        self.goals_scored += scored;
+        self.goals_conceded += conceded;
+    }
 }
 
 fn build_scores_table(results: String) -> HashMap<String, Team> {
@@ -39,6 +54,14 @@ fn build_scores_table(results: String) -> HashMap<String, Team> {
         // will be the number of goals conceded from team_2, and similarly
         // goals scored by team_2 will be the number of goals conceded by
         // team_1.
+        scores.entry(team_1_name.clone())
+            .or_insert(Team::new(team_1_name.clone()))
+            .update_scores(team_1_score, team_2_score);
+
+        // 更新 team_2 的得分和失分
+        scores.entry(team_2_name.clone())
+            .or_insert(Team::new(team_2_name.clone()))
+            .update_scores(team_2_score, team_1_score);
     }
     scores
 }
