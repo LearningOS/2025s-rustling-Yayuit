39fb42380ff1b13db673444b0f5450c4b60d86d5
Yayuit <33405544+Yayuit@users.noreply.github.com>
2025-04-02 09:33:39 +0000

    ...

--- exercises/iterators/iterators4.rs
+++ exercises/iterators/iterators4.rs
@@ -3,7 +3,6 @@
 // Execute `rustlings hint iterators4` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 pub fn factorial(num: u64) -> u64 {
     // Complete this function to return the factorial of num
@@ -15,6 +14,26 @@ pub fn factorial(num: u64) -> u64 {
     // For an extra challenge, don't use:
     // - recursion
     // Execute `rustlings hint iterators4` for hints.
+
+    let mut sum: u64 = 1;
+    // for i in 1..=num {
+    //     sum = sum * i;
+    // }
+    // return sum;
+
+    // if num == 1 || num == 0 {
+    //     return 1;
+    // }
+    // else {
+    //     return num * factorial(num - 1);
+    // }
+
+    // for i in 2..=num {
+    //     sum *= i;
+    // }
+    // sum
+
+    (1..=num).product()
 }
 
 #[cfg(test)]
--- exercises/iterators/iterators5.rs
+++ exercises/iterators/iterators5.rs
@@ -11,7 +11,6 @@
 // Execute `rustlings hint iterators5` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 use std::collections::HashMap;
 
@@ -35,7 +34,7 @@ fn count_for(map: &HashMap<String, Progress>, value: Progress) -> usize {
 fn count_iterator(map: &HashMap<String, Progress>, value: Progress) -> usize {
     // map is a hashmap with String keys and Progress values.
     // map = { "variables1": Complete, "from_str": None, ... }
-    todo!();
+    map.values().filter(|&&v| v == value).count()
 }
 
 fn count_collection_for(collection: &[HashMap<String, Progress>], value: Progress) -> usize {
@@ -54,8 +53,12 @@ fn count_collection_iterator(collection: &[HashMap<String, Progress>], value: Pr
     // collection is a slice of hashmaps.
     // collection = [{ "variables1": Complete, "from_str": None, ... },
     //     { "variables2": Complete, ... }, ... ]
-    todo!();
+    collection.iter() // 迭代切片中的每个 HashMap
+    .flat_map(|map| map.values()) // 获取每个 HashMap 的值
+    .filter(|&&v| v == value) // 过滤出与给定 value 相等的值
+    .count() // 计算匹配的数量
 }
+
 
 #[cfg(test)]
 mod tests {
